932a2de90295255c68e63e41dc4cb1b42dcf6c81
dahee kim <ekekekgml@naver.com>
2021-01-12 14:55:46 +0900

    style: eslint / prettier 적용

--- components/QuestionList.tsx
+++ components/QuestionList.tsx
@@ -2,27 +2,23 @@ import styled from 'styled-components';
 import QuestionListItem from './QuestionListItem';
 
 const Container = styled.div`
-/*Layout 적용 이후 width, margin 변경 요망* */
-    width : 60%;
-    margin-left: 20%;
-    margin-top: 200px;
-    margin-bottom: 200px;
-    background-color: var(--white-yellow);
-
+  /*Layout 적용 이후 width, margin 변경 요망* */
+  width: 60%;
+  margin-left: 20%;
+  margin-top: 200px;
+  margin-bottom: 200px;
+  background-color: var(--white-yellow);
 `;
 
-
-
-
-export default function QuestionList(){
-    return( 
-        <Container>
-            <QuestionListItem/>
-            <QuestionListItem/>
-            <QuestionListItem/>
-            <QuestionListItem/>
-            <QuestionListItem/>
-            <QuestionListItem/>
-        </Container>
-    )
+export default function QuestionList() {
+  return (
+    <Container>
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+      <QuestionListItem />
+    </Container>
+  );
 }
--- components/Questioner.tsx
+++ components/Questioner.tsx
@@ -1,33 +1,32 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-    width : 75px;
-    display: flex;
-    flex-direction: column;
-    justify-content : center;
-    align-items : left;
+  width: 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
 `;
 
 const Image = styled.img`
-    border-radius: 50%;
-    border: 1px solid rgba(0,0,0,0.2);
-    width : 75px;
-    height : 75px;
-    margin-bottom : 5px;
+  border-radius: 50%;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  width: 75px;
+  height: 75px;
+  margin-bottom: 5px;
 `;
 
-
 const Name = styled.div`
-    font-size: 15px;
-    font-weight: 700;
-    text-align :  center;
-`
+  font-size: 15px;
+  font-weight: 700;
+  text-align: center;
+`;
 
-export default function Questioner(){
-    return(
-        <Container>
-            <Image src="/images/sample.jpg" alt="sampleImage"/>
-            <Name>김다희님</Name>
-        </Container>
-    )
+export default function Questioner() {
+  return (
+    <Container>
+      <Image src="/images/sample.jpg" alt="sampleImage" />
+      <Name>김다희님</Name>
+    </Container>
+  );
 }
