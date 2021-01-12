8942e7e7a350282f050eac1f57f565d8c85a2f5f
toneyparky <qkrtmddhks95@gmail.com>
2021-01-12 11:48:44 +0900

    style: answer, layout css 설정

--- components/AnswerDetail.tsx
+++ components/AnswerDetail.tsx
@@ -14,15 +14,15 @@ export interface AnswerDetailProps {
 export function AnswerDetail({data}: AnswerDetailProps) {
   return data ? (
     <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.left}>
-          {/*<div className={styles.avatar}>*/}
-          {/*<img src="/jobs_avatar.png" alt="avatar" />*/}
-          {/*</div>*/}
-          <div className={styles.created}>{data.createdAt}</div>
+      {/*<div className={styles.top}>*/}
+      <div className={styles.left}>
+        <div className={styles.avatar}>
+          <img src="/jobs_avatar.png" alt="avatar" />
         </div>
+        <div className={styles.created}>{data.createdAt}</div>
       </div>
       <div className={styles.contents}>{data.contents}</div>
+      {/*</div>*/}
     </div>
   ) : (
     <>답변이 없다</>
