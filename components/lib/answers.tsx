ce63c7bdc8a631dd6837db450231aa52c4d663a1
toneyparky <qkrtmddhks95@gmail.com>
2021-01-13 19:40:54 +0900

    fix: Model을 이용하여 data를 넘기도록 수정

--- components/lib/answers.tsx
+++ components/lib/answers.tsx
@@ -1,4 +1,4 @@
-import {AnswerDetailProps} from '../AnswerDetail';
+import {AnswerModel} from '../../types/model';
 
 export function getAllPostIds() {
   return [
@@ -10,35 +10,29 @@ export function getAllPostIds() {
   ];
 }
 
-export async function getAnswersDetailData(
-  id: string
-): Promise<AnswerDetailProps[]> {
+export async function getAnswersDetailData(id: string): Promise<AnswerModel[]> {
   // TODO axios를 이용하여 서버로 정보를 가져와야 한다.
 
   return [
     {
-      data: {
-        id: parseInt(id),
-        questionId: 1,
-        contents:
-          '1번 답변입니다.\n' +
-          '이런 내용에 대해서 답변을 하고 있습니다.'.repeat(20),
-        responderId: null,
-        createdAt: '2021-01-11',
-        lastModifiedAt: '2021-01-11T12:32:22',
-      },
+      id: parseInt(id),
+      questionId: 1,
+      contents:
+        '1번 답변입니다.\n' +
+        '이런 내용에 대해서 답변을 하고 있습니다.'.repeat(20),
+      responderId: null,
+      createdAt: '2021-01-11',
+      lastModifiedAt: '2021-01-11T12:32:22',
     },
     {
-      data: {
-        id: parseInt(id),
-        questionId: 2,
-        contents:
-          '2번 답변입니다.\n' +
-          '이런 내용에 대해서 답변을 하고 있습니다.'.repeat(30),
-        responderId: null,
-        createdAt: '2021-01-12',
-        lastModifiedAt: '2021-01-12T12:32:22',
-      },
+      id: parseInt(id),
+      questionId: 2,
+      contents:
+        '2번 답변입니다.\n' +
+        '이런 내용에 대해서 답변을 하고 있습니다.'.repeat(30),
+      responderId: null,
+      createdAt: '2021-01-12',
+      lastModifiedAt: '2021-01-12T12:32:22',
     },
   ];
 }
--- components/lib/questions.tsx
+++ components/lib/questions.tsx
@@ -1,4 +1,4 @@
-import {QuestionDetailProps} from '../QuestionDetail';
+import {QuestionModel} from '../../types/model';
 
 export function getAllPostIds() {
   return [
@@ -12,17 +12,15 @@ export function getAllPostIds() {
 
 export async function getQuestionDetailData(
   id: string
-): Promise<QuestionDetailProps> {
+): Promise<QuestionModel> {
   // TODO axios를 이용하여 서버로 정보를 가져와야 한다.
 
   return {
-    data: {
-      id: parseInt(id),
-      title: '이런 내용에 대해서 질문을 하고 있습니다.'.repeat(3),
-      contents: '이런 내용에 대해서 질문을 하고 있습니다.'.repeat(50),
-      responderId: null,
-      createdAt: '2021-01-10',
-      lastModifiedAt: '2021-01-10T12:32:22',
-    },
+    id: parseInt(id),
+    title: '이런 내용에 대해서 질문을 하고 있습니다.'.repeat(3),
+    contents: '이런 내용에 대해서 질문을 하고 있습니다.'.repeat(50),
+    responderId: null,
+    createdAt: '2021-01-10',
+    lastModifiedAt: '2021-01-10T12:32:22',
   };
 }
