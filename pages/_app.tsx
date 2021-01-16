50070631b817882f3041b3e2e2b7f7e2f3b3498c
toneyparky <qkrtmddhks95@gmail.com>
2021-01-17 08:49:29 +0900

    feat: normalize.css 적용하여 브라우져간 css 종속성 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import {AppProps} from 'next/app';
+import 'normalize.css';
 
 function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />;
