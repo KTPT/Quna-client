389379432fbf258d80320515f0d62fe291f82583
dahee kim <ekekekgml@naver.com>
2021-01-18 01:51:35 +0900

    feat: getAllQuestions 예시 데이터 추가

--- components/lib/questions.tsx
+++ components/lib/questions.tsx
@@ -24,3 +24,37 @@ export async function getQuestionDetailData(
     lastModifiedAt: '2021-01-10T12:32:22',
   };
 }
+
+export async function getAllQuestions() {
+  //TODO axios를 이용하여 질문 리스트를 가져와야 한다.
+
+  const data = [
+    {
+      id: 1,
+      title: '1번 질문의 제목입니다.',
+      contents: '1번 질문의 내용입니다.',
+      responderId: null,
+      createdAt: '2021-01-10',
+      lastModifiedAt: '2021-01-10T12:32:22',
+    },
+    {
+      id: 2,
+      title: '2번 질문의 제목입니다.',
+      contents: '2번 질문의 내용입니다.',
+      responderId: null,
+      createdAt: '2021-01-10',
+      lastModifiedAt: '2021-01-10T12:33:33',
+    },
+    {
+      id: 3,
+      title: '3번 질문의 제목입니다.',
+      contents: '3번 질문의 내용입니다.',
+      responderId: null,
+      createdAt: '2021-01-10',
+      lastModifiedAt: '2021-01-10T12:33:33',
+    },
+  ];
+  console.log('getAllQuestions: ', data);
+
+  return {data};
+}
