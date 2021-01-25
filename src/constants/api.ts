f393c8048b5bd6698806a23ba2f39ed59a2b3188
toneyparky <qkrtmddhks95@gmail.com>
2021-01-25 21:19:04 +0900

    feat: 로그인 요청 보내고 토큰 추출

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,11 +1,13 @@
 const BASE_URL = 'http://localhost:8080';
 
 export const API = (
-  model: 'Question' | 'Answer' | string,
+  model: 'Question' | 'Answer' | 'Login' | string,
   questionsId?: string
 ) =>
   model === 'Question'
     ? `${BASE_URL}/questions`
     : model === 'Answer'
     ? `${BASE_URL}/questions/${questionsId}/answers`
+    : model === 'Login'
+    ? `${BASE_URL}/login`
     : '';
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import ApiButton from '../../components/ApiButton';
 import styled from 'styled-components';
 import Layout from '../../components/Layout';
+import {useState} from 'react';
+import axios from 'axios';
+import {API} from '../../constants/api';
 
-const Container = styled.div`
+const Container = styled.form`
   display: flex;
   flex-direction: column;
   width: 240px;
@@ -22,17 +25,51 @@ const Password = styled.div`
 `;
 
 const Login: React.FC = () => {
+  const [input, setInput] = useState({
+    nickname: '',
+    password: '',
+  });
+
+  const {nickname, password} = input;
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {value, name} = e.currentTarget;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const request = {...input};
+
+    const response = await axios.post(API('Login'), request);
+    console.log(response.data.token);
+  };
+
   return (
     <Layout>
       <h1>로그인</h1>
-      <Container>
+      <Container onSubmit={submit}>
         <Nickname>
           <div>닉네임</div>
-          <input type="text" name="nickname" />
+          <input
+            type="text"
+            name="nickname"
+            value={nickname}
+            onChange={onChange}
+          />
         </Nickname>
         <Password>
           <div>비밀번호</div>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
         </Password>
         <ApiButton content={'로그인 하기'} />
       </Container>
