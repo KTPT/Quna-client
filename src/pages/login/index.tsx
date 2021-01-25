8b17d5482fca35ac675effe725c2856a69bfbb27
toneyparky <qkrtmddhks95@gmail.com>
2021-01-25 23:01:22 +0900

    feat: 로그인 요청 성공과 실패에 대한 처리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import {useState} from 'react';
 import ApiButton from '../../components/ApiButton';
 import styled from 'styled-components';
 import Layout from '../../components/Layout';
-import {useState} from 'react';
 import axios from 'axios';
 import {API} from '../../constants/api';
+import {useRouter} from 'next/router';
+import {TOKEN} from '../../constants/token';
 
 const Container = styled.form`
   display: flex;
@@ -31,6 +33,7 @@ const Login: React.FC = () => {
   });
 
   const {nickname, password} = input;
+  const router = useRouter();
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     const {value, name} = e.currentTarget;
@@ -59,8 +62,16 @@ const Login: React.FC = () => {
     }
     const request = {...input};
 
-    const response = await axios.post(API('Login'), request);
-    console.log(response.data.token);
+    await axios
+      .post(API('Login'), request)
+      .then(response => {
+        localStorage.setItem(TOKEN, response.data.token);
+        alert('로그인되었습니다.');
+        router.push('/');
+      })
+      .catch(e => {
+        alert('로그인에 실패했습니다. 다시 시도해주세요.');
+      });
   };
 
   return (
