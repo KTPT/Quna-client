1d7b21641bcbb3f4cc2e59ec1259d01c14d660d8
toneyparky <qkrtmddhks95@gmail.com>
2021-01-27 17:41:36 +0900

    chore: 불필요한 import 제거

--- src/components/ApiButton.tsx
+++ src/components/ApiButton.tsx
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 import * as React from 'react';
-import Link from 'next/link';
-import {ParsedUrlQueryInput} from 'querystring';
 
 const Container = styled.button`
   max-width: 500px;
